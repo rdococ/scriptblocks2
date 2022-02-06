4d4de0c0c8100fbb68389cabe1c06fe9c8312613
rdococ <rd606six@googlemail.com>
2022-02-06 08:57:54 +0000

    Copy context when creating new process

--- blocks/processes.lua
+++ blocks/processes.lua
@@ -20,7 +20,7 @@ sb2.registerScriptblock("scriptblocks2:create_new_process", {
 	sb2_action = sb2.simple_action {
 		action = function (pos, node, process, frame, context)
 			local dirs = sb2.facedirToDirs(node.param2)
-			return sb2.Process:new(sb2.Frame:new(vector.add(pos, dirs.right), frame:getContext()), process:getHead(), process:getStarter(), process:isDebugging())
+			return sb2.Process:new(sb2.Frame:new(vector.add(pos, dirs.right), frame:getContext():copy()), process:getHead(), process:getStarter(), process:isDebugging())
 		end
 	},
 })
